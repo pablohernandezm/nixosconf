4ac9959cbd85a507a77da474b223358285061481
pablohernandezm <phernandezm07@gmail.com>
2025-07-20 02:44:24 -0500

    feat(boot): change systemd boot loader for grub
    This doesn't fix anything but I liked it a little more.

--- modules/nixos/system/boot.nix
+++ modules/nixos/system/boot.nix
@@ -1,8 +1,15 @@
 { pkgs, ... }:
 {
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    efi.canTouchEfiVariables = true;
+
+    grub = {
+      enable = true;
+      devices = [ "nodev" ];
+      efiSupport = true;
+      useOSProber = true;
+    };
+  };
 
   # Use zen kernel.
   boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen;
