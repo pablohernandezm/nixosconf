5ddc1ef21b6dd91e72f5081c5ce799323f7e824d
pablohernandezm <phernandezm07@gmail.com>
2025-07-20 02:39:50 -0500

    fix(app): proton-ge into steam extraCompatPackages
    Avoid depending on protonup and manual update of
    proton-ge.

--- modules/nixos/desktop/steam/default.nix
+++ modules/nixos/desktop/steam/default.nix
@@ -3,25 +3,22 @@
   environment.systemPackages = with pkgs; [
     steam-run-free
     wine-wayland
-    protonup-ng
   ];
 
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-  };
-
-  hardware = {
-    graphics = {
+  programs = {
+    steam = {
       enable = true;
-      enable32Bit = true;
+      # remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      # dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+      # localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+      extraCompatPackages = with pkgs; [ proton-ge-bin ];
     };
 
-    amdgpu.amdvlk = {
+    # Some games may distribute on Linux as an AppImage.
+    # To get them to execute via Steam, enable AppImage with the binfmt setting
+    appimage = {
       enable = true;
-      support32Bit.enable = true;
+      binfmt = true;
     };
   };
 }
