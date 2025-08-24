483225642f4f0733a328c794d97bc01b332ba27a
pablohernandezm <phernandezm07@gmail.com>
2025-08-23 19:39:47 -0500

    Fix: Media keys settings for Hyprland
    Fix the mute keymap and modify the raise and lower volume bindings to
    cancel mute when the events are triggered.

--- modules/home/desktop/hyprland/default.nix
+++ modules/home/desktop/hyprland/default.nix
@@ -140,9 +140,8 @@ in
       # l -> do stuff even when locked
       # e -> repeats when key is held
       bindle = [
-        ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-        ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
-        ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ", XF86AudioRaiseVolume, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ 0 && wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+        ", XF86AudioLowerVolume, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ 0 && wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
         ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
         ", XF86AudioPlay, exec, playerctl play-pause"
         ", XF86AudioNext, exec, playerctl next"
